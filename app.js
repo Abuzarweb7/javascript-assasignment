48814d0d8eeb561b5c0611f3c8f02717ea81ab0a
hammad-sohail <hammadsohail733@gmail.com>
2025-06-30 17:12:37 +0500

    Alert, Prompt, is done

---
+++ app.js
@@ -0,0 +1,20 @@
+question1
+var username =prompt("Enter your name");
+alert(" Welcome " +  username )
+question2
+var username = prompt("Enter your name");
+var age = prompt("Enter your age");
+ console.log( username + " is "  + age + " years  old ") 
+question3
+var favcolor = prompt("Enter your fav color");
+alert( "Wow,!" + favcolor + " is  a  nice  color " )
+question4
+var num1 = +prompt (4);
+var num2 = +prompt (5);
+var total = (num1 + num2)
+console .log ("The result of " + num1 +  "+" + num2 + " is " +  total + ".") 
+question5
+var username = prompt("Enter your name");
+var country = prompt("Enter your country");
+var hobby = prompt("Enter your hobby");
+alert( " Hi " + " i,m " +  username  + " from " + country +  " and  i  love"   +  hobby )
