f3e587a0b4f214588e7f0c07afdcb8cca19a0e22
hammad-sohail <hammadsohail733@gmail.com>
2025-07-01 12:53:40 +0500

    assassignment 2 done

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-question1
-var username =prompt("Enter your name");
-alert(" Welcome " +  username )
-question2
-var username = prompt("Enter your name");
-var age = prompt("Enter your age");
- console.log( username + " is "  + age + " years  old ") 
-question3
-var favcolor = prompt("Enter your fav color");
-alert( "Wow,!" + favcolor + " is  a  nice  color " )
-question4
-var num1 = +prompt (4);
-var num2 = +prompt (5);
-var total = (num1 + num2)
-console .log ("The result of " + num1 +  "+" + num2 + " is " +  total + ".") 
-question5
-var username = prompt("Enter your name");
-var country = prompt("Enter your country");
-var hobby = prompt("Enter your hobby");
-alert( " Hi " + " i,m " +  username  + " from " + country +  " and  i  love"   +  hobby )
+// question1
+// var username =prompt("Enter your name");
+// alert(" Welcome " +  username )
+// question2
+// var username = prompt("Enter your name");
+// var age = prompt("Enter your age");
+//  console.log( username + " is "  + age + " years  old ") 
+// question3
+// var favcolor = prompt("Enter your fav color");
+// alert( "Wow,!" + favcolor + " is  a  nice  color " )
+// question4
+// var num1 = +prompt (4);
+// var num2 = +prompt (5);
+// var total = (num1 + num2)
+// console .log ("The result of " + num1 +  "+" + num2 + " is " +  total + ".") 
+// question5
+// var username = prompt("Enter your name");
+// var country = prompt("Enter your country");
+// var hobby = prompt("Enter your hobby");
+// alert( " Hi " + " i,m " +  username  + " from " + country +  " and  i  love"   +  hobby )
+
+
+
+
+
+
+// String concatenation Practice:
+//  Task 1:
+// var address = prompt("Enter your street address");
+// var city = prompt("Enter your city");
+// var country = prompt ("Enter your conntry");
+// console.log("your full adrees is :" + address  +   city  +  country)
+// // TASK 2
+// var username  = prompt("Enter your name");
+// var favnumber = prompt("Enter your fav number");
+// console.log("your new  user name is : "  +    username  +  favnumber )
+// Task 3
+// var firstname = prompt("Enter your first name");
+// var lastname = prompt ("Enter your last name");
+// console.log(     firstname       +     lastname +    "@gmail.com");
+// Task 4
+// var favcolor = prompt("Enter your fav color");
+// var favanimal = prompt("Enter your fav animal");
+// var favfood = prompt("Enter your fav food");
+// console.log(   "  Your favorite things are:  "    +    favcolor   +  ""  + favanimal   +    "and"   +    favfood  )
+// Task5
+// var quote = prompt("Enter your quit name");
+// var author = prompt("Enter your author name  ");
+// console.log("The only limit is your mind ." +  " - " + quote + " " + author)
